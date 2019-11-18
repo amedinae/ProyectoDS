855ad6d1395342b426d3e618339df560c41cabe4
Juan Diego Plaza <jplazag@unal.edu.co>
2019-11-17 23:16:49 -0500

    Lectura de JSON de javascript listay generación del JSON que leerá el javascript

--- Procesamiento.py
+++ Procesamiento.py
@@ -16,37 +16,8 @@ def setHoras(curso, dia, primeraHora, ultimaHora):
 
 
 def leerMaterias():
-    data = []
-    with open('10680.json') as a:
-        data.append(json.load(a))  # Dinámica
-
-    with open('16809.json') as b:
-        data.append(json.load(b))  # Digital
-
-    with open('18473.json') as c:
-        data.append(json.load(c))  # Señales 2
-
-    with open('21949.json') as d:
-        data.append(json.load(d))  # Intensive
-
-    with open('22969.json') as e:
-        data.append(json.load(e))  # Gráfica interactiva
-
-    # with open('16706.json', encoding='utf-8') as q:
-    # 	data.append(json.load(q))
-
-    # with open('16707.json', encoding='utf-8') as g:
-    # 	data.append(json.load(g))
-
-    # with open('16809.json') as h:
-    # 	data.append(json.load(h))
-
-    data[0].update({"materia": "DINAMICA", "C_M": 5, "C_G": [1, 2, 3, 4]})
-    data[1].update({"materia": "DIGITAL", "C_M": 4, "C_G": [1, 2, 3, 4, 5, 6, 7, 8]})
-    data[2].update({"materia": "SEÑALES II", "C_M": 3, "C_G": [x for x in range(data[2]['count'])]})
-    data[3].update({"materia": "INTENSIVE", "C_M": 2, "C_G": [x for x in range(data[3]['count'])]})
-    data[4].update({"materia": "GRAFICA INTERACTIVA", "C_M": 1, "C_G": [x for x in range(data[4]['count'])]})
-
+    with open('calificados.json') as a:
+        data = json.load(a)  # Dinámica
     return data
 
 
@@ -81,7 +52,7 @@ def organizarHorario(data):
                                 ultimaHora = 10 * int(data[k]['list'][i]['week'][j][h][3]) + int(
                                     data[k]['list'][i]['week'][j][h][4])
                         horario = setHoras(horario, j, primeraHora, ultimaHora)
-            grupo.append({'horario': horario, 'grupo': i, 'asignatura': data[k]['materia'], 'C_M': data[k]['C_M'],
+            grupo.append({'horario': horario, 'grupo': i, 'asignatura': data[k]['materia'], 'codigo': data[k]['code'], 'C_M': data[k]['C_M'],
                           'C_G': data[k]['C_G'][i]})
         listaGrupos.append(grupo)
 
@@ -125,7 +96,7 @@ def combinatoria(lista):
 # listas[i][j]['asignatura'] --------------------- Toma el nombre de una asignatura ------------------------------------
 
 def sumarListas(listas):
-    resultado = [{'horario': horarioVacio(), 'grupo': [], 'asignatura': [], 'C_H': 0} for x in range(len(listas))]
+    resultado = [{'horario': horarioVacio(), 'grupo': [], 'codigo': [], 'asignatura': [], 'C_H': 0} for x in range(len(listas))]
     horarios = BinaryHeap(len(listas))
 
     for i in range(len(resultado)):
@@ -143,8 +114,9 @@ def sumarListas(listas):
                 break
             resultado[i]['grupo'].append(listas[i][j]['grupo'])
             resultado[i]['asignatura'].append(listas[i][j]['asignatura'])
+            resultado[i]['codigo'].append(listas[i][j]['codigo'])
         if sirve:
-            horarios.insert(resultado[i]['C_H'], {'asignaturas': resultado[i]['asignatura'], 'grupos': resultado[i]['grupo']})
+            horarios.insert(resultado[i]['C_H'], {'asignaturas': resultado[i]['asignatura'], 'codigos': resultado[i]['codigo'], 'grupos': resultado[i]['grupo']})
     return horarios
 
 
@@ -163,16 +135,10 @@ if __name__ == '__main__':
     print("¿Cuántos horarios quieres visualizar?")
     n = int(input())
     cola_ordenada = prioridadHorario(algo, n)
+    cola_final = []
     for x in range(n):
-        print(cola_ordenada.dequeue())
+        cola_final.append(cola_ordenada.dequeue()) #Conversión a objeto serializable, para que se pueda transformar en un JSON
 
-# print(data)
-# print(combinatoria(listaGrupos)[1])
-# printHorario(sumarListas(combinatoria(listaGrupos))[0]['horario'])
-# print(sumarListas(combinatoria(listaGrupos))[0]['horario'])
-# print(sumarListas(combinatoria(listaGrupos)))
-# algo = sumarListas(combinatoria(listaGrupos))
-# with open('data.json', 'w') as f:
-#     json.dump(algo,f)
-# print(listaGrupos[0][1]['horario'])
-# print(listaGrupos)
+    with open('data.json', 'w') as f:
+        json.dump(cola_final, f)
+
